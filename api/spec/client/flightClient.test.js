5c604098d4710171e71d7a17977f023c282a9d03
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-02-28 15:39:08 +0000

    Test passed for catching error

--- api/spec/client/flightClient.test.js
+++ api/spec/client/flightClient.test.js
@@ -8,7 +8,16 @@ describe('FlightClient', () => {
     const client = new FlightClient()
     fetch.mockResponseOnce(JSON.stringify(mockResponse))
     client.loadFlights().then((flights) => {
-      expect(flights).toEqual(mockResponse)
+      // buckets[1] an array that refers the cheapest flights portion of the api response
+      expect(flights).toEqual(mockResponse.data.buckets[1])
+    })
+  })
+
+  it('returns an error if request is not fulfilled', () => {
+    const client = new FlightClient()
+    fetch.mockReject('Error')
+    client.loadFlights().then((error) => {
+      expect(error).toEqual('Error')
     })
   })
 })
