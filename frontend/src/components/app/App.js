e2006120370c45e7afff2d21dc6abb8186030ad9
ShaunFlood <shaunflood@hotmail.co.uk>
2023-03-01 12:11:17 +0000

    css-and-moving-files

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,26 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState, useEffect } from 'react';
-import Form from '../form/Form';
+import React from 'react';
+import FlightForm from '../../components/form/Form';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-
-        </a>
-      </header>
-      <Form />
+    <div>
+      <FlightForm />
     </div>
   );
 }
--- frontend/src/components/form/form.js
+++ frontend/src/components/form/form.js
@@ -1,110 +1,100 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import { Form } from 'react-router-dom';
-import styles from './form.css';
+import React from 'react';
+import FlightClient from '../../client/flightClient'
+import './form.css';
 
-// import the backend thing that works - with the api key stuff
-function FlightSearch() {
-    const [formInput, setFormInput] = useState({
-        inboundDestination: '',
-        outboundDestination: '',
-        departureDate: '',
-        returnDate: '',
-        numberOfTravellers: ''
-    });
-    handleNumberOfTravellersChange = (event) => {
-        this.setState({ numberOfTravellers: event.target.value });
-      }
-    
-      handleOutboundDestinationChange = (event) => {
-        this.setState({ outboundDestination: event.target.value });
-      }
-    
-      handleInboundDestinationChange = (event) => {
-        this.setState({ inboundDestination: event.target.value });
-      }
-    
-      handleDepartureDateChange = (event) => {
-        this.setState({ departureDate: event.target.value });
-      }
-    
-      handleReturnDateChange = (event) => {
-        this.setState({ returnDate: event.target.value });
-      }
+class FlightForm extends React.Component {
+  state = {
+    numberOfTravellers: '',
+    outboundDestination: '',
+    inboundDestination: '',
+    departureDate: '',
+    returnDate: ''
+  }
 
-      handleSubmit = (event) => {
-        event.preventDefault();
-        const { numberOfTravellers, outboundDestination, inboundDestination, departureDate, returnDate } = this.state;
-        FlightClient.loadFlights(numberOfTravellers, outboundDestination, inboundDestination, departureDate, returnDate)
-          .then((flights) => {
-            // handle the API response
-          })
-          .catch((error) => {
-            // handle the error
-          });
-      }
+  handleNumberOfTravellersChange = (event) => {
+    this.setState({ numberOfTravellers: event.target.value });
+  }
 
+  handleOutboundDestinationChange = (event) => {
+    this.setState({ outboundDestination: event.target.value });
+  }
+
+  handleInboundDestinationChange = (event) => {
+    this.setState({ inboundDestination: event.target.value });
+  }
+
+  handleDepartureDateChange = (event) => {
+    this.setState({ departureDate: event.target.value });
+  }
+
+  handleReturnDateChange = (event) => {
+    this.setState({ returnDate: event.target.value });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { numberOfTravellers, outboundDestination, inboundDestination, departureDate, returnDate } = this.state;
+    FlightClient.loadFlights(numberOfTravellers, outboundDestination, inboundDestination, departureDate, returnDate)
+      .then((flights) => {
+        console.log('API response:', flights);
+        // handle the API response
+      })
+      .catch((error) => {
+        // handle the error
+      });
+  }
+
+
+  render() {
+    const { numberOfTravellers, outboundDestination, inboundDestination, departureDate, returnDate } = this.state;
     return (
-        <div className={styles.container}>
-            <div className={styles.card}>
-                <div className={styles.row}>
-                    <div className={styles.col12}>
-                        <h4>Flight</h4>
-                        <span className={styles.textMuted}>Travel the world with us</span>
-                    </div>
-
-
-                    
-
-                    <div className={styles.colLg6 + ' ' + styles.colMd12}>
-                       <div className={styles.formFloating}>
-                            <input type="text" className={styles.formControl} placeholder="FLYING FROM" />
-                            <label>FLYING FROM</label>
-                        </div>
-                    </div>
-                    <div className={styles.colLg6 + ' ' + styles.colMd12}>
-                        <div className={styles.formFloating}>
-                            <input type="text" className={styles.formControl} placeholder="FLYING TO" />
-                            <label>FLYING TO</label>
-                        </div>
-                    </div>
-                    <div className={styles.colLg6 + ' ' + styles.colMd12}>
-                        <div className={styles.formFloating}>
-                            <input type="date" className={styles.formControl} placeholder="DEPARTING" />
-                            <label>DEPARTING DATE</label>
-                        </div>
-                    </div>
-                    <div className={styles.colLg6 + ' ' + styles.colMd12}>
-                        <div className={styles.formFloating}>
-                            <input type="date" className={styles.formControl} placeholder="RETURNING" />
-                            <label>RETURNING DATE</label>
-                        </div>
-                    </div>
-                    <div className={styles.colLg4 + ' ' + styles.colMd12}>
-                        <div className={styles.formFloating}>
-                            <select className={styles.formSelect}>
-                                <option selected hidden>Open this select menu</option>
-                                <option value="1">One</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
-                                <option value="4">Four</option>
-                                <option value="5">Five</option>
-                                <option value="6">Six</option>
-                                <option value="7">Seven</option>
-                                <option value="8">Eight</option>
-                            </select>
-                            <label>ADULTS(18+)</label>
-                        </div>
-                    </div>
-                    <div className={styles.col12 + ' ' + styles.mt4}>
-                        <button className={styles.btn + ' ' + styles.btnPrimary + ' ' + styles.textUppercase} type="submit" onClick={handleSubmit}>Submit<i className="fa fa-plane ms-3"></i></button>
-                    </div>
-                </div>
-            </div>
+    <main class="content">
+        <h3 class="panel__title">Find a Flight</h3>
+      <form onSubmit={this.handleSubmit}>
+        <div class="form__row">
+        <div class="form__input">
+        <label>
+          Outbound destination:
+          </label>
+          <input className="form-control" type="text" value={outboundDestination} onChange={this.handleOutboundDestinationChange} />
         </div>
+        </div>
+        <br />
+        <div class="form__row">
+        <div class="form__input">
+        <label>Inbound destination:</label>
+          <input className="form-control" type="text" value={inboundDestination} onChange={this.handleInboundDestinationChange} />
+        </div>
+        </div>
+        <br />
+        <div class="form__row">
+        <div class="form__input">
+        <label>Departure date:</label>
+          <input className="form-control" type="date" value={departureDate} onChange={this.handleDepartureDateChange} />
+        </div>
+        </div>
+        <br />
+        <div class="form__row">
+        <div class="form__input">
+        <label>Return date:</label>
+          <input className="form-control" type="date" value={returnDate} onChange={this.handleReturnDateChange} />
+        </div>
+        </div>
+        <br />
+        <div class="form__row">
+        <div class="form__input">
+        <label>
+          Number of travellers:
+          <input type="number" value={numberOfTravellers} onChange={this.handleNumberOfTravellersChange} />
+        </label>
+        </div>
+        </div>
+        <br />
+        <button type="submit">Search flights</button>
+      </form>
+    </main>
     );
+  }
 }
 
-ReactDOM.render(<FlightSearch />, document.getElementById('root'));
-
-export default Form;
+export default FlightForm;
