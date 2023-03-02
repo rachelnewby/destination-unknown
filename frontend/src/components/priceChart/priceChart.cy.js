2d9723cad0b371a9de063783186478a4191d7459
Rachel Newby <rachelcnewby@gmail.com>
2023-03-02 10:29:11 +0000

    Testing for chart

--- frontend/src/components/priceChart/priceChart.cy.js
+++ frontend/src/components/priceChart/priceChart.cy.js
@@ -13,15 +13,5 @@ describe("PriceChart", () => {
     cy.mount(<PriceChart chartData={mockData}/>)
     cy.get('#bar-chart').should('be.visible')
   })
-
-  // it('displays carrierLabels on the page', () => {
-  //   cy.visit('/path/to/PriceChart') // replace with actual path to component
-  //   cy.get('#chart-title').should('contain', 'Here\'s your flight info')
-  //   cy.get('#bar-chart').should('exist')
-  //   cy.get('#bar-chart').should('have.attr', 'data-labels').then(labelsAttr => {
-  //     const labels = JSON.parse(labelsAttr)
-  //     expect(labels).to.include.members(['BA - Magic Carpet', 'Monarch - Concord', 'Malaysian Air - Wizz Air'])
-  //   })
-  // })
 })
 
--- frontend/src/components/priceChart/priceChart.js
+++ frontend/src/components/priceChart/priceChart.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'; 
+import React from 'react'; 
 import { Bar } from 'react-chartjs-2';
 import {CategoryScale} from 'chart.js';
 import Chart from 'chart.js/auto';
