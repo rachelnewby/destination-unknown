53a454c5544d96271bb686e5cec0b67bf5e7baf5
Rachel Newby <rachelcnewby@gmail.com>
2023-03-03 13:45:02 +0000

    Cypress unit test for form component passed

--- frontend/src/components/form/form.cy.js
+++ frontend/src/components/form/form.cy.js
@@ -3,38 +3,17 @@ import FlightForm from './form'
 describe('FlightForm', () => {
   it('user can enter dept, arr dates, arr/dept locations and traveller number', () => {
     cy.mount(<FlightForm />)
-    cy.get('.form__row:nth-child(1) .form-control').click();
-    cy.get('.form__row:nth-child(1) .form-control').type('LAX');
-    cy.get('.form__row:nth-child(3) .form-control').click();
-    cy.get('.form__row:nth-child(3) .form-control').type('FCO');
-    cy.get('.form__row:nth-child(5) .form-control').click();
-    cy.get('.form__row:nth-child(5) .form-control').type('2023-07-01');
-    cy.get('.form__row:nth-child(7) .form-control').click();
-    cy.get('.form__row:nth-child(7) .form-control').type('2023-07-08');
-    cy.get('.form__row:nth-child(9) .form-control').click();
-    cy.get('.form__row:nth-child(9) .form-control').type('1');
+    cy.get('.form__row:nth-child(1) .form-control').select('LHR');
+    cy.get('.form__row:nth-child(3) .form-control').select('BCN');
+    cy.get('.form__row:nth-child(5) .form-control').type('2023-03-04');
+    cy.get('.form__row:nth-child(7) .form-control').type('{backspace}');
+    cy.get('.form__row:nth-child(7) .form-control').type('{backspace}');
+    cy.get('.form__row:nth-child(7) .form-control').type('{backspace}');
+    cy.get('.form__row:nth-child(7) .form-control').type('{backspace}');
+    cy.get('.form__row:nth-child(7) .form-control').type('2023-03-10');
+    cy.get('.form__row:nth-child(9) .form-control').type('2');
     cy.get('button').click();
   })
-
-  // it('console logs the correct information', () => {
-  //   cy.mount(<FlightForm />)
-  //   cy.get('.form__row:nth-child(1) .form-control').click();
-  //   cy.get('.form__row:nth-child(1) .form-control').type('LAX');
-  //   cy.get('.form__row:nth-child(3) .form-control').click();
-  //   cy.get('.form__row:nth-child(3) .form-control').type('FCO');
-  //   cy.get('.form__row:nth-child(5) .form-control').click();
-  //   cy.get('.form__row:nth-child(5) .form-control').type('2023-07-01');
-  //   cy.get('.form__row:nth-child(7) .form-control').click();
-  //   cy.get('.form__row:nth-child(7) .form-control').type('2023-07-08');
-  //   cy.get('.form__row:nth-child(9) .form-control').click();
-  //   cy.get('.form__row:nth-child(9) .form-control').type('1');
-  //   cy.get('button').click();
-  //   cy.get('form').submit();
-  //   cy.window().then((win) => {
-  //     cy.spy(win.console, 'log').as('consoleLogSpy');
-  //     cy.get('@consoleLogSpy').should("number of travellers " + "1")
-  //   });
-  // })
 })
 
 
