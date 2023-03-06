67271f4fdbc3fbffa69681d1c9be95fc8938f46d
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-03-06 17:58:15 +0000

    toggle added to homepage with css

--- frontend/src/components/form/form.js
+++ frontend/src/components/form/form.js
@@ -1,34 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import './form.css';
-import PriceChart from '../priceChart/priceChart';
-import ErrorMessage from '../errorMessage/errorMessage';
-
+import React, { useState, useEffect } from "react";
+import "./form.css";
+import PriceChart from "../priceChart/priceChart";
+import ErrorMessage from "../errorMessage/errorMessage";
 
 const inbound = [
-  {name: "paris-france", airportcode: "CDG", place: 'Paris'},
-  {name: "bologna-italy", airportcode: "BLQ", place: 'Bologna'},
-  {name: "rome-italy", airportcode: "FCO", place: 'Rome'},
-  {name: "barcelona-spain", airportcode: "BCN", place: 'Barcelona'},
-  {name: "ibiza-spain", airportcode: "IBZ", place: 'Ibiza'},
-  {name: "lisbon-portugal", airportcode: "LIS", place: 'Lisbon'},
-  {name: "porto-portugal", airportcode: "OPO", place: 'Porto'},
-  {name: "copenhagen-denmakr", airportcode: "CPH", place: 'Copenhagen'},
-  {name: "budapest-hungary", airportcode: "BUD", place: 'Budapest'},  
-  {name: "prague-czech-republic", airportcode: "PRG", place: 'Prague'},
-  {name: "athens-greece", airportcode: "ASH", place: 'Athens'},
-  {name: "krakow-poland", airportcode: "KRK", place: 'Krakow'}
-]
+  { name: "paris-france", airportcode: "CDG", place: "Paris" },
+  { name: "bologna-italy", airportcode: "BLQ", place: "Bologna" },
+  { name: "rome-italy", airportcode: "FCO", place: "Rome" },
+  { name: "barcelona-spain", airportcode: "BCN", place: "Barcelona" },
+  { name: "ibiza-spain", airportcode: "IBZ", place: "Ibiza" },
+  { name: "lisbon-portugal", airportcode: "LIS", place: "Lisbon" },
+  { name: "porto-portugal", airportcode: "OPO", place: "Porto" },
+  { name: "copenhagen-denmakr", airportcode: "CPH", place: "Copenhagen" },
+  { name: "budapest-hungary", airportcode: "BUD", place: "Budapest" },
+  { name: "prague-czech-republic", airportcode: "PRG", place: "Prague" },
+  { name: "athens-greece", airportcode: "ASH", place: "Athens" },
+  { name: "krakow-poland", airportcode: "KRK", place: "Krakow" },
+];
 
 const outbound = [
-  {place: "Heathrow", airportcode: "LHR"},
-  {place: "Manchester", airportcode: "MAN"},
-  {place: "Cardiff", airportcode: "CWL"},
-  {place: "Bristol", airportcode: "BRS"},
-  {place: "Edinburgh", airportcode: "EDI"},
-  {place: "Dublin", airportcode: "DUB"}
-]
+  { place: "Heathrow", airportcode: "LHR" },
+  { place: "Manchester", airportcode: "MAN" },
+  { place: "Cardiff", airportcode: "CWL" },
+  { place: "Bristol", airportcode: "BRS" },
+  { place: "Edinburgh", airportcode: "EDI" },
+  { place: "Dublin", airportcode: "DUB" },
+];
 
-function FlightForm(clickFunction) {
+function FlightForm() {
   const [numberOfTravellers, setNumberofTravellers] = useState("");
   const [outboundDestination, setOutboundDestination] = useState("");
   const [inboundDestination, setInboundDestination] = useState("");
@@ -44,103 +43,134 @@ function FlightForm(clickFunction) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const selectedInbound = inbound.find((option) => option.airportcode === inboundDestination);
+    const selectedInbound = inbound.find(
+      (option) => option.airportcode === inboundDestination
+    );
     const url = `http://localhost:4000/?city=${selectedInbound.name}&travellers=${numberOfTravellers}&outbound=${outboundDestination}&inbound=${selectedInbound.airportcode}&departureDate=${departureDate}&returnDate=${returnDate}`;
     console.log(url);
-  
-    fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Sorry, an error has occurred. Please try again.');
-      }
-      return response.json();
-    })
-    .then(data => {
-      if (data.flights.items && data.flights.items.length > 0) {
-        console.log(data);
-        setFlights(data.flights);
-        console.log('flights', data);
-        setCity(data.city);
-        console.log('city', data.city)
-        setError(null);
-      } else {
-        throw new Error('Sorry, there are no flights matching your request. Please try again.');
-      }
-    })
-    .catch(error => {
-      console.log('Error:', error);
-      setError(`${error}`);
-    });
-    console.log("search clicked");
-    clickFunction.onButtonClick();
-  }
-    return (
-      <main class="content">
-          <h3 class="panel__title">Find a Flight</h3>
-        <form onSubmit={handleSubmit}>
-          <div class="form__row">
-          <div class="form__input">
-          <div class="form__row">
-          <div class="form__input">
-          <label>Outbound destination:</label>
-          <select className="form-control" value={outboundDestination} onChange={(event) => setOutboundDestination(event.target.value)}>
-                <option value="">Select destination</option>
-                {outbound.map((option) => (
-                  <option key={option.airportcode} value={option.airportcode}>{option.place}</option>
-                ))}
-              </select>
-          </div>
-          </div>
-          </div>
-          </div>
-          <br />
-          <div class="form__row">
-          <div class="form__input">
-          <label>Inbound destination:</label>
-          <select className="form-control" value={inboundDestination} onChange={(event) => setInboundDestination(event.target.value)}>
-                <option value="">Select destination</option>
-                {inbound.map((option) => (
-                  <option key={option.airportcode} value={option.airportcode}>{option.place}</option>
-                ))}
-              </select>
-          </div>
-          </div>
-          <br />
-          <div class="form__row">
-          <div class="form__input">
-          <label>Departure date:</label>
-            <input className="form-control" type="text" placeholder="YYYY-MM-DD" value={departureDate} onChange={(event) => setDepartureDate(event.target.value)} />
-          </div>
-          </div>
-          <br />
-          <div class="form__row">
-          <div class="form__input">
-          <label>Return date:</label>
-            <input className="form-control" type="text" placeholder="YYYY-MM-DD" value={returnDate} onChange={(event) => setReturnDate(event.target.value)} />
-          </div>
-          </div>
-          <br />
-          <div class="form__row">
-          <div class="form__input">
-          <label>
-          Number of travellers:
-            </label>
-            <input className="form-control" type="text" value={numberOfTravellers} onChange={(event) => setNumberofTravellers(event.target.value)} />
-          </div>
-          </div>
-          <br />
-          <button type="submit">Search flights</button>
-        </form>
-        {flights && <PriceChart chartData={flights} />}
-        {/* {city && <div> city data is that = {city}</div>} */}
-        {error && <ErrorMessage error={error}/>}
-      </main>
-    );
-  };
 
-  export default FlightForm;
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Sorry, an error has occurred. Please try again.");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.flights.items && data.flights.items.length > 0) {
+          console.log(data);
+          setFlights(data.flights);
+          console.log("flights", data);
+          setCity(data.city);
+          console.log("city", data.city);
+          setError(null);
+        } else {
+          throw new Error(
+            "Sorry, there are no flights matching your request. Please try again."
+          );
+        }
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+        setError(`${error}`);
+      });
+  };
+  return (
+    <main class="content">
+      <h3 class="panel__title">Find a Flight</h3>
+      <form onSubmit={handleSubmit}>
+        <div class="form__row">
+          <div class="form__input">
+            <div class="form__row">
+              <div class="form__input">
+                <label>Outbound destination:</label>
+                <select
+                  className="form-control"
+                  value={outboundDestination}
+                  onChange={(event) =>
+                    setOutboundDestination(event.target.value)
+                  }
+                >
+                  <option value="">Select destination</option>
+                  {outbound.map((option) => (
+                    <option key={option.airportcode} value={option.airportcode}>
+                      {option.place}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br />
+        <div class="form__row">
+          <div class="form__input">
+            <label>Inbound destination:</label>
+            <select
+              className="form-control"
+              value={inboundDestination}
+              onChange={(event) => setInboundDestination(event.target.value)}
+            >
+              <option value="">Select destination</option>
+              {inbound.map((option) => (
+                <option key={option.airportcode} value={option.airportcode}>
+                  {option.place}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <br />
+        <div class="form__row">
+          <div class="form__input">
+            <label>Departure date:</label>
+            <input
+              className="form-control"
+              type="text"
+              placeholder="YYYY-MM-DD"
+              value={departureDate}
+              onChange={(event) => setDepartureDate(event.target.value)}
+            />
+          </div>
+        </div>
+        <br />
+        <div class="form__row">
+          <div class="form__input">
+            <label>Return date:</label>
+            <input
+              className="form-control"
+              type="text"
+              placeholder="YYYY-MM-DD"
+              value={returnDate}
+              onChange={(event) => setReturnDate(event.target.value)}
+            />
+          </div>
+        </div>
+        <br />
+        <div class="form__row">
+          <div class="form__input">
+            <label>Number of travellers:</label>
+            <input
+              className="form-control"
+              type="text"
+              value={numberOfTravellers}
+              onChange={(event) => setNumberofTravellers(event.target.value)}
+            />
+          </div>
+        </div>
+        <br />
+        <button type="submit">Search flights</button>
+      </form>
+      {flights && <PriceChart chartData={flights} />}
+      {/* {city && <div> city data is that = {city}</div>} */}
+      {error && <ErrorMessage error={error} />}
+    </main>
+  );
+}
+
+export default FlightForm;
