9c9a1a47dc3f05a8c6c8911ec7784d41c30312ed
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-03-06 12:07:53 +0000

    first two tests passed for knownfor component. Displays heading and a div to contain data

---
+++ frontend/src/components/knownFor/knownFor.cy.js
@@ -0,0 +1,14 @@
+import mockRoadGoatData from "../roadGoatStatsChart/mockRoadGoatData";
+import KnownFor from "./knownFor.js";
+
+describe("KnownFor", () => {
+  it("has a title for Known for", () => {
+    cy.mount(<KnownFor goatData={mockRoadGoatData} />);
+    cy.get("#knownfor-title").should("exist");
+    cy.get("#knownfor-title").should("have.text", "KNOWN FOR");
+  });
+  it("a div exists that will then contain our spans", () => {
+    cy.mount(<KnownFor goatData={mockRoadGoatData} />);
+    cy.get("#emoji-div").should("exist");
+  });
+});
---
+++ frontend/src/components/knownFor/knownFor.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const KnownFor = ({ goatData }) => {
+  return (
+    <div>
+      <h3 id="knownfor-title">KNOWN FOR</h3>
+      <div id="emoji-div"></div>
+    </div>
+  );
+};
+
+export default KnownFor;
