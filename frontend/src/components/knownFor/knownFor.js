2af9d1567c71f869a4e53e2669f51f84480e0153
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-03-06 12:28:45 +0000

    foreach written to extract the data from the response. Loop also fixes incorrect url's provided

--- frontend/src/components/knownFor/knownFor.js
+++ frontend/src/components/knownFor/knownFor.js
@@ -1,6 +1,28 @@
 import React, { useState } from "react";
 
 const KnownFor = ({ goatData }) => {
+  // console.log(goatData.included[2]);
+  const emojiData = [];
+
+  goatData.included.forEach((element) => {
+    if (element.type === "known_for") {
+      emojiData.push({
+        description: element.attributes.name,
+        emoji: element.attributes.icon + "-48.png",
+      });
+    }
+  });
+
+  emojiData.forEach((element) => {
+    console.log(element);
+  });
+
+  // while type is === 'known_for
+  // get name and icon url
+  // each icon url need "-48.png" added to the end
+  // add to array of objects
+  // iterate over and pass values into span
+
   return (
     <div>
       <h3 id="knownfor-title">KNOWN FOR</h3>
