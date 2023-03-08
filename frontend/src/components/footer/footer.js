f6aa797ac7608ffeaad9ccb99b4065c5bdb48820
UserChi <117730509+UserChi@users.noreply.github.com>
2023-03-08 16:13:36 +0000

    have done the footer need to add logo but i've added the links to the github

--- frontend/src/components/footer/footer.js
+++ frontend/src/components/footer/footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import Logo from '../../assets/img-logo.png'
 import './footer.css';
 
 const Footer = () => {
@@ -10,27 +11,26 @@ const Footer = () => {
       <section className="footer-info">
         <section className="footer-info-left">
           <section className="footer-info__name">
-            copyright ©  Destination Unknown 2023
+            ©  Destination Unknown 2023
           </section>
         </section>
         <section className="footer-info-center">
-          <section className="footer-info__terms">
-            <a href="/privacy-policy">Privacy Policy</a>
-            <br />
-            <a href="/terms-of-service">Terms of Service</a>
+          <section className="footer-info__logo">
+          {/* <NavLink to='/'><img src={Logo} alt="logo" /></NavLink> */}
+            <a href="/privacy-policy">logo</a>
           </section>
         </section>
         <section className="footer-info-right">
-          <section className="footer-info__contact">
-            <a href="/about-us">About Us</a>
-            <br />
-            <a href="/contact-us">Contact Us</a>
+          <section className="footer-info__team">
+            Adam Hoar <a href="https://github.com/amh4" target="_blank" rel="noopener noreferrer">GitHub</a>  Chioma Igwe <a href="https://github.com/UserChi" target="_blank" rel="noopener noreferrer">GitHub</a>   Millie Severino <a href="https://github.com/MillieKS" target="_blank" rel="noopener noreferrer">GitHub</a>  Rachel Newby <a href="https://github.com/rachelnewby" target="_blank" rel="noopener noreferrer">GitHub</a>   Shaun Flood <a href="https://github.com/ShaunFlood" target="_blank" rel="noopener noreferrer">GitHub</a>
+
+
           </section>
         </section>
       </section>
       <hr className="footer-seperator" />
       <section className="footer-bottom">
-        <p>{`© ${year} My Awesome Website. All Rights Reserved.`}</p>
+        <p>{`© ${year} The Tonalist Squad. All Rights Reserved.`}</p>
       </section>
     </footer>
   );
