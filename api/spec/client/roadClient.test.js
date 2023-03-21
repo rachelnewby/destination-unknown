cd605de226106f2480452137741604b8349189fd
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-03-21 17:09:07 +0000

    added a backend test to cover error response from RG api

--- api/spec/client/roadClient.test.js
+++ api/spec/client/roadClient.test.js
@@ -1,16 +1,24 @@
-const RoadgoatClient = require('../../client/roadgoatClient');
-const mockRoadResponse = require('./mockRoadgoat')
+const RoadgoatClient = require("../../client/roadgoatClient");
+const mockRoadResponse = require("./mockRoadgoat");
 
-require('jest-fetch-mock').enableMocks();
+require("jest-fetch-mock").enableMocks();
 
-describe('RoadgoatClient', () => {
-  it('calls fetch and returns city information from API', () => {
-    const client = new RoadgoatClient()
-    fetch.mockResponseOnce(JSON.stringify(mockRoadResponse))
-    const city = 'barcelona-spain';
+describe("RoadgoatClient", () => {
+  it("calls fetch and returns city information from API", () => {
+    const client = new RoadgoatClient();
+    fetch.mockResponseOnce(JSON.stringify(mockRoadResponse));
+    const city = "barcelona-spain";
     client.loadCity(city).then((city) => {
-    console.log('roadgoat client', mockRoadResponse.data)
-    expect(city).toEqual(mockRoadResponse.data)
-   })
-  })
-})
+      console.log("roadgoat client", mockRoadResponse.data);
+      expect(city).toEqual(mockRoadResponse.data);
+    });
+  });
+
+  it("returns an error if request is not fulfilled", () => {
+    const client = new RoadgoatClient();
+    fetch.mockReject("Error");
+    client.loadCity().then((error) => {
+      expect(error).toEqual("Error");
+    });
+  });
+});
