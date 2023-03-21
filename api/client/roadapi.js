cc1c3fd2cbf9c7b18da939a0a63ef39a95bc0533
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-03-21 18:02:15 +0000

    added to gitignore

--- api/client/roadapi.js
+++
@@ -1,3 +0,0 @@
-const roadApi = '9534be2a29a76b4e4d7dd79eb00b382a'
-
-module.exports = roadApi;
--- api/client/roadsecret.js
+++
@@ -1,3 +0,0 @@
-const roadSecret = '92cc093f7e3cc6774be30495f30b21d7'
-
-module.exports = roadSecret;
